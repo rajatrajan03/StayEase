c12328f0c23eaef3496ea90236f0af8c9fa3e04f
rajatrajan03 <rajatagnihotri03@outlook.com>
2026-04-24 15:02:46 +0530

    yess

--- StayEase/stayease/urls.py
+++ StayEase/stayease/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+import os
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -12,4 +13,8 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# Vercel runs with DEBUG=False; add media serving route for uploaded images.
+if os.getenv("VERCEL") == "1":
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
